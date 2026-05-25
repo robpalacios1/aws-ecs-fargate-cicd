12d86b5c4d3fa3f200eaf5562affdc56683bc08a
roberto <robpalacios1@hotmail.com>
2026-05-25 11:05:28 -0500

    Create Route Table Association Resources

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -130,3 +130,27 @@ resource "aws_route_table" "private_rt" {
     environment = "development"
   }
 }
+
+# ====================================================================
+# 5. Create Route Table Association
+# ====================================================================
+
+resource "aws_route_table_association" "public_rta_1" {
+  subnet_id = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rta_2" {
+  subnet_id = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_rta_1" {
+  subnet_id = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rta_2" {
+  subnet_id = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
