3e2dfc0caa6ecdec7bb29460bc73920287b39ee4
roberto <robpalacios1@hotmail.com>
2026-05-25 10:11:37 -0500

    Create Public Subnets Resource

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -9,3 +9,23 @@ resource "aws_vpc" "main_vpc" {
     environment = "dev"
   }
 }
+
+# ====================================================================
+# 2. Create Public Subnets 
+# ====================================================================
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "public_subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "publi_subnet_2"
+  }
+}
