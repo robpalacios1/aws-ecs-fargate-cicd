dfe1002f1c10b6d99ebfbd8ad5f71697184f72b2
roberto <robpalacios1@hotmail.com>
2026-05-25 10:19:32 -0500

    Add availability zone for subnets

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -17,6 +17,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "public_subnet_1"
   }
@@ -25,8 +26,9 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
   tags = {
-    Name = "publi_subnet_2"
+    Name = "public_subnet_2"
   }
 }
 
@@ -37,6 +39,7 @@ resource "aws_subnet" "public_subnet_2" {
 resource "aws_subnet" "private_subnet_1" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "private_subnet_1"
   }
@@ -45,6 +48,7 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "private_subnet_1"
   }
