31ffda5a5188356605f8f3a85efa4a2ed5fc5f23
roberto <robpalacios1@hotmail.com>
2026-05-25 11:21:50 -0500

    Create Public Subnets ID's Output

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -6,3 +6,15 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value = aws_vpc.main_vpc.id
 }
+
+# ====================================================================
+# 2. Public Subnets ID 
+# ====================================================================
+
+output "public_subnets_ids" {
+  description = "List of ID's Public Subnets"
+  value = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
+}
