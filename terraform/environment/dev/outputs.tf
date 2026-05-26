484ffa7fcabed8a6fc5523e872b6a8e5e90d6973
roberto <robpalacios1@hotmail.com>
2026-05-26 10:41:54 -0500

    Add Security Module Outputs

--- terraform/environment/dev/outputs.tf
+++ terraform/environment/dev/outputs.tf
@@ -24,3 +24,27 @@ output "private_subnets_ids" {
   description = "List of ID's Private Subnets"
   value       = module.networking.private_subnets_ids
 }
+
+# ====================================================================
+# 4. Security Module Outputs
+# ====================================================================
+
+output "ecs_task_execution_role_arn" {
+  description = "The ARN of the ECS task execution role"
+  value       = module.security.ecs_task_execution_role_arn
+}
+
+output "ecs_task_role_arn" {
+  description = "The ARN of the ECS task role"
+  value       = module.security.ecs_task_role_arn
+}
+
+output "alb_security_group_id" {
+  description = "The ID of the ALB security group"
+  value       = module.security.alb_security_group_id
+}
+
+output "ecs_tasks_security_group_id" {
+  description = "The ID of the ECS tasks security group"
+  value       = module.security.ecs_tasks_security_group_id
+}
