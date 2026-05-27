4808660028045c1d630eb30815aa52d2718bbbf0
roberto <robpalacios1@hotmail.com>
2026-05-27 10:58:46 -0500

    indentation code with terraform fmt command

--- terraform/modules/alb/variables.tf
+++ terraform/modules/alb/variables.tf
@@ -5,7 +5,7 @@
 variable "main_alb_name" {
   description = "The name of the ALB"
   type        = string
-  default = "dev-main-alb"
+  default     = "dev-main-alb"
 }
 
 variable "main_alb_security_group_id" {
@@ -21,7 +21,7 @@ variable "main_alb_subnets" {
 variable "main_alb_environment" {
   description = "The environment of the ALB"
   type        = string
-  default = "dev"
+  default     = "dev"
 }
 
 # ====================================================================
@@ -31,13 +31,13 @@ variable "main_alb_environment" {
 variable "main_tg_name" {
   description = "The name of the target group"
   type        = string
-  default = "dev-ecs-target-group"
+  default     = "dev-ecs-target-group"
 }
 
 variable "main_tg_port" {
   description = "The port of the target group"
   type        = number
-  default = 80
+  default     = 80
 }
 
 variable "vpc_id" {
@@ -52,17 +52,17 @@ variable "vpc_id" {
 variable "main_listener_port" {
   description = "The port of the listener"
   type        = string
-  default = "80"
-} 
+  default     = "80"
+}
 
 variable "main_listener_protocol" {
   description = "The protocol of the listener"
   type        = string
-  default = "HTTP"
+  default     = "HTTP"
 }
 
 variable "main_listener_name" {
   description = "The name of the listener"
   type        = string
-  default = "dev-main-listener"
+  default     = "dev-main-listener"
 }
