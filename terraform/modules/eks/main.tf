6dbe23494c793e55f9e8311074930c89e63065ea
roberto <robpalacios1@hotmail.com>
2026-06-03 12:17:42 -0500

    indent code with terraform fmt

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -3,29 +3,29 @@
 # ====================================================================
 
 resource "aws_iam_role" "eks_cluster_role" {
-    name = "dev-eks-cluster-role"
+  name = "dev-eks-cluster-role"
 
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17",
-        Statement = [
-            {
-                Action = "sts:AssumeRole",
-                Effect = "Allow",
-                Principal = {
-                    Service = "eks.amazonaws.com"
-                }
-            }
-        ]
-    })
-    tags = {
-        Name = "dev-eks-cluster-role"
-        environment = "dev"
-    }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+  tags = {
+    Name        = "dev-eks-cluster-role"
+    environment = "dev"
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-    role       = aws_iam_role.eks_cluster_role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 # ====================================================================
@@ -33,39 +33,39 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 # ====================================================================
 
 resource "aws_iam_role" "eks_node_role" {
-    name = "dev-eks-node-role"
+  name = "dev-eks-node-role"
 
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17",
-        Statement = [
-            {
-                Action = "sts:AssumeRole",
-                Effect = "Allow",
-                Principal = {
-                    Service = "ec2.amazonaws.com"
-                }
-            }
-        ]
-    })
-    tags = {
-        Name = "dev-eks-node-role"
-        environment = "dev"
-    }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+  tags = {
+    Name        = "dev-eks-node-role"
+    environment = "dev"
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-    role       = aws_iam_role.eks_node_role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-    role       = aws_iam_role.eks_node_role.name
-    policy_arn = "arn:aws:iam::aws:policy/  AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/  AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "ecr_read_only_policy" {
-    role       = aws_iam_role.eks_node_role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 
@@ -74,21 +74,21 @@ resource "aws_iam_role_policy_attachment" "ecr_read_only_policy" {
 # ====================================================================
 
 resource "aws_eks_cluster" "main_eks" {
-    name = "dev-eks"
-    role = aws_iam_role.eks_cluster_role.arn
+  name = "dev-eks"
+  role = aws_iam_role.eks_cluster_role.arn
 
-    vpc_config {
-        subnet_ids = [
-            "subnet-0123456789public-1",
-            "subnet-0123456789public-2",
-            "subnet-0123456789private-1",
-            "subnet-0123456789private-2",
-        ]
-    }
-
-    depends_on = [
-        aws_iam_role_policy_attachment.eks_cluster_policy
+  vpc_config {
+    subnet_ids = [
+      "subnet-0123456789public-1",
+      "subnet-0123456789public-2",
+      "subnet-0123456789private-1",
+      "subnet-0123456789private-2",
     ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
 }
 
 # ====================================================================
@@ -96,27 +96,27 @@ resource "aws_eks_cluster" "main_eks" {
 # ====================================================================
 
 resource "aws_eks_node_group" "main_nodes" {
-    cluster_name    = aws_eks_cluster.main.eks.name
-    node_group_name = "dev-eks-nodes"
-    node_role_arn   = aws_iam_role.eks_node_role.arn
+  cluster_name    = aws_eks_cluster.main.eks.name
+  node_group_name = "dev-eks-nodes"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
 
-    subnet_ids = [
-        "subnet-0123456789private-1",
-        "subnet-0123456789private-2",
-    ]
-    scaling_config {
-        desired_size = 2
-        max_size     = 3
-        min_size     = 1
-    }
+  subnet_ids = [
+    "subnet-0123456789private-1",
+    "subnet-0123456789private-2",
+  ]
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
 
-    depends_on = [
-        aws_iam_role_policy_attachment.eks_worker_node_policy,
-        aws_iam_role_policy_attachment.eks_cni_policy,
-        aws_iam_role_policy_attachment.ecr_read_only_policy,
-    ]
-    tags = {
-        Name = "dev-eks-nodes"
-        environment = "dev"
-    }
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ecr_read_only_policy,
+  ]
+  tags = {
+    Name        = "dev-eks-nodes"
+    environment = "dev"
+  }
 }
